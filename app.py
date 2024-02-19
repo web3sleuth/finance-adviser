d3e842d95994b74f878a65d2719e647492540934
web3sleuth <149721671+web3sleuth@users.noreply.github.com>
2024-02-19 15:35:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import streamlit as st
+from streamlit_login_auth_ui.widgets import __login__
 
+st.set_page_config(
+    page_title="Finance Adviser")
+
+st.header("Finance Adviser")
+
+__login__obj = __login__(
+    auth_token = st.secrets.email_api_key,
+    company_name = "Finance Adviser",
+    width = 200, height = 250, 
+    logout_button_name = 'Logout', hide_menu_bool = False, 
+    hide_footer_bool = False, 
+    lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
+
+LOGGED_IN = __login__obj.build_login_ui()
+
+if LOGGED_IN == True:
+    st.markdown("Your Streamlit Application Begins here!")
